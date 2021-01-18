d0af0e4863760a0d26331fb14becadb32c353340
Karl Lankester <kzdrenka@gmail.com>
2021-01-18 11:48:52 +0000

    stuff

--- lib/test_app-stack.ts
+++ lib/test_app-stack.ts
@@ -1,9 +1,23 @@
 import * as cdk from '@aws-cdk/core';
-
+import {
+  App,
+  CustomRule,
+  GitHubSourceCodeProvider,
+  RedirectStatus,
+} from "@aws-cdk/aws-amplify";
 export class TestAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
+    const amplifyApp = new App(this, "MyApp", {
+      sourceCodeProvider: new GitHubSourceCodeProvider({
+        owner: "amplience",
+        repository: "Zdrenka/test_app",
+        oauthToken: cdk.SecretValue.secretsManager(
+          "arn:aws:secretsmanager:eu-west-1:395026163603:secret:thundercats-github-pat-amplify-oWNvcc"
+        ),
+      }),
+    });
+    amplifyApp.addBranch("master");
   }
+
 }
